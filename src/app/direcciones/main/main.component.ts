94ba749f23b1724b9016c7d878a0b756a625f84d
GalaxyAYL <josealbertoayalazumba@gmail.com>
2023-01-09 08:21:20 -0500

    Login v4.5.0 - MejorasDiseño

--- src/app/direcciones/main/main.component.ts
+++ src/app/direcciones/main/main.component.ts
@@ -20,10 +20,10 @@ export class MainComponent implements OnInit {
   public iPhone4s:Producto = new Producto("5","iPhone 4s","Lanzado en octubre de 2011", 1,199);
   public iPhone5:Producto = new Producto("6","iPhone 5","Lanzado en septiembre del 2012", 1,250);
   public iPhone5S:Producto = new Producto("7","iPhone 5S","Lanzado en septiembre de 2013", 1,250);
-  public iPhone5C:Producto = new Producto("8","iPhone5C","Lanzado en septiembre de 2013", 1,649);
+  public iPhone5C:Producto = new Producto("8","iPhone 5C","Lanzado en septiembre de 2013", 1,649);
   public iPhone6:Producto = new Producto("9","iPhone 6","Lanzado en septiembre del 2014", 1,199);
   public iPhone6Plus:Producto = new Producto("10","iPhone 6 Plus","Lanzado en septiembre del 2014", 1,299);
-  public iPhone6sPlus:Producto = new Producto("11","iPhone 6s Plus","iPhone 6s Plus", 1,599);
+  public iPhone6sPlus:Producto = new Producto("11","iPhone 6s Plus","Lanzado en septiembre del 2015", 1,599);
 
 //MIS VARIABLES Y OBJETOS
 public disponibles:Producto[]=[this.iPhone,this.iPhone3G,this.iPhone3GS,this.iPhone4,this.iPhone4s,this.iPhone5,this.iPhone5S
@@ -37,8 +37,16 @@ producto:number //FILAS DE LOS PRODUCTOS
 
 public matriz: Array<Array<String>>=[
   ['HDC-001','iPhone (1.ª generación)','Lanzado en junio de 2007','200'],
-  ['2','iPhone 3G','Lanzado en junio de 2007','299'],
-  ['3','iPhone 3GS','Lanzamiento junio de 2009','368']
+  ['HDC-002','iPhone 3G','Lanzado en junio de 2007','299'],
+  ['HDC-003','iPhone 3GS','Lanzamiento junio de 2009','368'],
+  ['HDC-004','iPhone 4','Lanzado en octubre de 2011', '300'],
+  ['HDC-005','iPhone 4s','Lanzado en sept. del 2012','300'],
+  ['HDC-006','iPhone 5','Lanzamiento junio de 2009','250'],
+  ['HDC-007','iPhone 5S','Lanzado en septiembre de 2013','250'],
+  ['HDC-008','iPhone 5C','Lanzado en septiembre de 2013','600'],
+  ['HDC-009','iPhone 6','Lanzado en septiembre del 2014','199'],
+  ['HDC-010','iPhone 6 Plus','Lanzado en septiembre del 2014','299'],
+  ['HDC-011','iPhone 6s Plus','Lanzado en septiembre del 2015','599']
 ]
 
   //MIS METODOS
